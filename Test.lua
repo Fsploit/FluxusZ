2520b2383c6c2ca1507390aa77ecbcc361caec78
Fsploit (retarded as fuck) <169143383+Fsploit@users.noreply.github.com>
2024-10-22 14:49:37 +0800

    Create Test.lua

---
+++ Test.lua
@@ -0,0 +1,24 @@
+getgenv = true
+local hwid =({https://www.google.com/search?client=ms-android-oppo-rvo2&sca_esv=546aec7a0f9e1974&sxsrf=ADLYWII5nBGhS2i4UXq2M2I7kYTnzgtP3Q:1729578475262&q=transfer+advertising+examples&udm=2&fbs=AEQNm0AAii1lAhsPaaai4wByEaaipRAHWexzqXOes0FJK1_cCNcUFPPaxlyqBmr552nx3oLBdewXO5TQvVoinXe_GS1rK7ji4g1Xf3vSZMpJFSloxl5p6tf9FRnQko7JTEf4tUmf4U3xuP_7XT204tv8tffIukB4KZ8csB91G6UnP-YTyZpZP8o24MqwF3v1fLCYABhzcUS9sMA4z-YqpOHRz9E-BroOjg&sa=X&ved=2ahUKEwjF4oP1raGJAxV5r1YBHRIvELwQtKgLegQIDRAB&biw=360&bih=702&dpr=2#vhid=yhY1cvencVF4kM&vssid=mosaic})
+
+if getgenv = true 
+  then do
+    print("fluxuszloaderupdated")
+  else
+    print("fuckingyes")
+  end
+end
+
+local val = false
+if val = true
+  then do
+  print("key_hwid_T0stEd_$&1683_9!73")
+  else
+    print("fluxus")
+    wait(100)
+    then do
+      getgenv = false
+    end
+  end
+end
+
